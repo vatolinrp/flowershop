986c6221366a8ef66c04d54dcab0f00660381e3b
Rostislav Vatolin <vatolinrp@mail.ru>
2015-09-18 09:22:37 +0300

    db testing

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -1,11 +1,17 @@
 package com.mkyong.web.controller;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.net.UnknownHostException;
 
 @Controller
 public class HelloController {
@@ -21,9 +27,36 @@ public class HelloController {
 	@RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
 	public ModelAndView hello(@PathVariable("name") String name) {
 
+		boolean auth = false;
+		try
+		{
+			MongoClient mongoClient = new MongoClient("127.6.49.4",27017);
+			DB db = mongoClient.getDB("flowers");
+			auth = db.authenticate("admin", "NfAc-CBXGWN9".toCharArray());
+			if(auth)
+			{
+				DBCollection table = db.getCollection("TEST_COLLECTION");
+				BasicDBObject document = new BasicDBObject();
+				document.put("id", 101);
+				document.put("value", "some_value");
+				table.insert(document);
+			}
+		}
+		catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
+
 		ModelAndView model = new ModelAndView();
 		model.setViewName("hello");
-		model.addObject("msg", name);
+		if(auth)
+		{
+			model.addObject("msg", "done");
+		}
+		else
+		{
+			model.addObject("msg", "not done");
+		}
 
 		return model;
 
